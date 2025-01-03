c8434c0d3dabec0a142837e812a0ab16aec3d74a
David Hernandez <48605281+jdhernandezS1@users.noreply.github.com>
2025-01-03 15:41:33 +0000

    app style and structure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Recipes from "./pages/Recipes";
-
+import NavBar from "./components/navBar/NavBar";
+import style from "./App.module.css"
 const App = () => {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/recipes">Recipes</Link>
-            </li>
-          </ul>
-        </nav>
+    <>
+    <NavBar/>
+    <main className={style.App} >
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/recipes" element={<Recipes />} />
-        </Routes>
-      </div>
-    </Router>
+    </main>
+    </>
   );
 };
 
