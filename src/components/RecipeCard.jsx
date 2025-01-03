708d673223eff9f7fea6d3a74d0590b601cac8e9
David Hernandez <48605281+jdhernandezS1@users.noreply.github.com>
2025-01-03 15:41:46 +0000

    feature: recipe card structure

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from '../styles/recipes/recipeCard.module.css'; // Import css modules stylesheet as styles
+
+
+const RecipeCard = ({ recipe }) => {
+    return (
+        <div className={styles.recipeCard}>
+            <a href={recipe.sourceUrl} target="blank">
+            <h3>{recipe.title}</h3>
+            <img src={recipe.image} alt={recipe.title} />
+            </a>
+        </div>
+    );
+};
+
+export default RecipeCard;
