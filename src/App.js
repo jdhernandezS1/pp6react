7b6bf10a5af6e0d8fca9119416be708227f2ed1c
David Hernandez <48605281+jdhernandezS1@users.noreply.github.com>
2025-01-06 22:10:13 +0000

    Routing in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from "react";
-import NavBar from "./components/navBar/NavBar";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import AppStyle from "./App.module.css"
+import StyleMain from "./styles/base.module.css"
+import Style from "./styles/NavBar/navBar.module.css"
 
 const App = () => {
   return (
     <>
-    <NavBar/>
+      <Router className={AppStyle.General}>
+        <p className={Style.Title}><span className={Style.TitleOne}>veg </span><span  className={Style.TitleTwo}> | world</span></p>
+        <div className={Style.navBar}>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/recipes">Recipes</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <main className={StyleMain.Main}>
+          <div className={StyleMain.Routes}>
+          <Routes >
+            <Route path="/" element={<Home />} />
+            <Route path="/recipes" element={<Recipes />} />
+          </Routes>
+          </div>
+        </main>
+      </Router>
     </>
   );
 };
