0e1703bea54b4811e401d0793d1e447802e4aa44
David Hernandez <48605281+jdhernandezS1@users.noreply.github.com>
2025-01-07 12:12:14 +0000

    Footer Styles

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import style from "../styles/footer/Footer.module.css"
+
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="footer-container">
-                <div className="footer-logo">
-                    <h2>Tu Empresa</h2>
+        <footer className={style["footer"]}>
+            <div className={style["footer-container"]}>
+                <div className={style["footer-logo"]}>
+                    <h2>Fit World</h2>
                     <p>&copy; 2025 Todos los derechos reservados</p>
                 </div>
-                <div className="footer-links">
+                <div className={style["footer-links"]}>
                     <h3>Links</h3>
                     <ul>
                         <li><Link to="/">Inicio</Link></li>
@@ -18,16 +20,16 @@ const Footer = () => {
                         <li><Link to="trainer">trainer</Link></li>
                     </ul>
                 </div>
-                <div className="footer-social">
+                <div className={style["footer-social"]}>
                     <h3>Conéctate</h3>
-                    <div className="social-icons">
+                    <div className={style["social-icons"]}>
                         <a href="https://www.facebook.com/"><i className="fab fa-facebook"></i></a>
                         <a href="https://www.facebook.com/"><i className="fab fa-twitter"></i></a>
                         <a href="https://www.facebook.com/"><i className="fab fa-instagram"></i></a>
                         <a href="https://www.facebook.com/"><i className="fab fa-linkedin"></i></a>
                     </div>
                 </div>
-                <div className="footer-contact">
+                <div className={style["footer-contact"]}>
                     <h3>Contacto</h3>
                     <p>Email: @.com</p>
                     <p>Tel: +123 456 789</p>
