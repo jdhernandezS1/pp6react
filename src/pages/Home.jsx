d4d1405a1177c53ace45e54a134b35d3ee12a3f4
David Hernandez <48605281+jdhernandezS1@users.noreply.github.com>
2025-01-07 15:23:56 +0000

    responsive design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import styles from "../styles/home/home.module.css"
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to Recipe Finder</h1>
+    <div className={styles["first-section"]}>
+      <h1 className={styles["home-title"]}>Welcome to Fit | WORLD</h1>
       <p>Find recipes and explore delicious options!</p>
     </div>
   );
