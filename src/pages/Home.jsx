b876637bd31577e2efa2336f9720f0e2a1b617e9
David Hernandez <48605281+jdhernandezS1@users.noreply.github.com>
2025-01-15 18:14:52 +0000

    home video style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ const Home = () => {
   return (
     <div className={styles["first-section"]}>
       <h1 className={styles["home-title"]}>Welcome to Fit | WORLD</h1>
-      <p>Find recipes and explore delicious options!</p>
+      <p>Find recipes, routines, calculate your daily calories and explore some options!</p>
+      <iframe className={styles["home-video"]} src="https://www.youtube.com/embed/_JRefJH6N00?si=YJ3smqtYGtSmUf-o&amp;controls=0&amp;start=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   );
 };
