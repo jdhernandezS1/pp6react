fee6199826f3095c84f3e50c612a821db4451e01
David Hernandez <48605281+jdhernandezS1@users.noreply.github.com>
2025-01-20 13:28:00 +0000

    Toggle menu for phone navbar

--- src/navBar/NarBarPhone.jsx
+++ src/navBar/NarBarPhone.jsx
@@ -1,34 +1,69 @@
-import Style from "../styles/navBar/navBar.module.css"
+import React, { useState } from "react";
+import Style from "../styles/navBar/navBar.module.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faBars, faTimes,faDumbbell, faHouse } from "@fortawesome/free-solid-svg-icons";
 
 const NavBarPhone = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
+
         <div className={Style["nav-bar-phone"]}>
-            <Link to="/" className={Style["link"]}> <p className={Style["title"]}><span className={Style["title-one"]}>Fit </span><span className={Style["title-two"]}> | world</span></p></Link>
-            <div className={Style["nav-bar"]}>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/"><FontAwesomeIcon icon={faLinkedin} /></Link>
-                        </li>
-                        <li>
-                            <Link to="recipes"> <FontAwesomeIcon icon={faLinkedin} />
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="calculator"><FontAwesomeIcon icon={faLinkedin} /></Link>
-                        </li>
-                        <li>
-                            <Link to="workouts"><FontAwesomeIcon icon={faLinkedin} /></Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+            <nav>
+
+                <div className={Style["header"]}>
+                    <Link to="/" className={Style["link"]}>
+                        <p className={Style["title"]}>
+                        
+                        <span className={Style["title-one"]} > 
+                        <FontAwesomeIcon icon={faDumbbell} /> Fit | world 
+                        </span>
+                        
+                        </p>
+                    </Link>
+                    <button
+                        className={Style["menu-toggle"]}
+                        onClick={toggleMenu}
+                        aria-label="Toggle menu"
+                    >
+                        <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
+                    </button>
+                </div>
+                {menuOpen && (
+                    <div className={Style["nav-bar"]}>
+                        <ul>
+                            <li>
+                                <Link to="/" onClick={toggleMenu}>
+                                <FontAwesomeIcon icon={faHouse} />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/recipes" onClick={toggleMenu}>
+                                    Recipes
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/calculator" onClick={toggleMenu}>
+                                    Calculator
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/workouts" onClick={toggleMenu}>
+                                    Workouts
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                )}
+            </nav>
+
         </div>
     );
 };
-
 
 export default NavBarPhone;
